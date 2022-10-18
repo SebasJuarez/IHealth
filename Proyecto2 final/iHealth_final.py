734f6ba760486fda078df34ff7017970a9ade2f1
Sebastian Juárez <84484212+SebasJuarez@users.noreply.github.com>
2022-10-18 13:19:51 -0600

    version 1.3 + diagrama ER

--- Proyecto2 final/iHealth_final.py
+++ Proyecto2 final/iHealth_final.py
@@ -3,6 +3,7 @@
 #Creación de un porgrama que maneja a los usuarios de IHealth +
 import sys
 import psycopg2
+from datetime import datetime
 conn= psycopg2.connect(database="Proyecto2",
     user='postgres',
     password='Fac3viche',
@@ -23,9 +24,11 @@ def main():
     if (opcion1 == "2"):
         Crearcuenta()
     if (opcion1 == "3"):
-        sys.exit
+        print("Feliz día!")
+        exit()
     else:
-        print("numero equivocado") 
+        print("numero equivocado")
+        main()
 
 #--------cuentas: inicio de sesion y creacion de las cuentas----
 
@@ -78,14 +81,17 @@ def Crearcuenta():
     cursor.execute("select user_id from usuario where nombre=%s and apellido = %s",(nombre,apellido))
     user_id=cursor.fetchone()[0]
     print("cuenta creada exitosamente\nSu numero de usuario (que debe ingresar en el inicio de sesión) es: %s"%(user_id))
-    print("Se le llevará al inicio de sesión")
-    Inicio()
+    if acctype=="Administrador":
+        print("Se le llevará al inicio de sesión")
+        Inicio()
+    else:
+        suscripcion()
 
 def Admin():
     print("-----Permisos de administrador consedidos------")
     print("-----Menu de administrador------")
     print("Que desea hacer?: ")
-    print("1. Modificar instructores\n2. Modificar sesiones\n3. Modificar usuarios")
+    print("1. Modificar instructores\n2. Modificar sesiones\n3. Modificar usuarios\n4. Regresar al inicio")
     opcion1=input()
     if opcion1=="1":
         print("Elija entre las opciones:")
@@ -174,14 +180,144 @@ def Admin():
         main()
 
 def Miembro():
-    
-    main()
+    print("Este estas son las opciones que se le ofrecen:")
+    print("1. Busqueda de sesiones\n2. Consultas de calendario o historial\n3. Estadisticas\n4. Peso semanal\n5. Configuracion\n6. volver al inicio")
+    opcion=input()
+    if opcion=="1":
+        busqueda()
+    if opcion=="2":
+        consultas()
+    if opcion=="5":
+        configuracion()  
+    if opcion=="6":
+        main()
 
 def suscripcion():
     print("Para usar estos servicios, debe utilizar seleccionar una suscripcion: ")
-    print("1. Oro\n2. Diamante")
-    opcion3=input("Elija uno para ver una descripción mas detallada")
-    Miembro()
+    print("1. Oro\n2. Diamante\n3. Realizar la compra")
+    opcion3=input("Elija uno para ver una descripción mas detallada: ")
+    if opcion3=="1":
+        print("Con su suscripción oro, se le ofrecen las sesiones 24/7\ny el prestamo de un smartwatch para el registro del entrenamiento\n(este se debe retornar en caso de baja en el plan) con un contrato mínimo de 12 meses\nEl precio es de Q250.00 mensuales")
+        suscripcion()
+    if opcion3=="2":
+        print("Con su suscripción diamante, se le ofrecen las sesiones 24/7\nUna sesión con nuestra nutricionista 1 vez al mes\ny el regalo de un smartwatch para el registro del entrenamiento\ncon un contrato mínimo de 12 meses\nEl precio es de Q500.00 mensuales")
+        suscripcion()
+    if opcion3=="3":
+        print("-----Compra de suscripcion------")
+        opcion4=input("Que tipo de suscripción elíge:\n1. Oro\n2. Diamante\n")
+        if opcion4=="1":
+            tipo="oro"
+            costo="250"
+        elif opcion4=="2":
+            tipo="diamante"
+            costo="500"
+        else:
+            print("no esta entre las opciones...")
+            suscripcion()
+        user=input("Numero de usuario (se le asigno al crear la cuenta): ")
+        pago=input("Como desea pagar su suscripcion (debito o credito): ")
+        estado="activo"
+        fecha_init=datetime.today().strftime('%d-%m-%Y')
+        fecha_fin=datetime.today().strftime('%d-%m-%Y')
+        cursor.execute("insert into suscripcion (user_id, tipo, estado, costo, tipo_pago, fecha_init, fecha_fin) values (%s, %s, %s, %s, %s, %s, %s)", (user, tipo, estado, costo, pago, fecha_init, fecha_fin))
+        print("transaccion realizada con exito!\nBienvenido!")
+        Miembro()
+def configuracion():
+            print("-----Configuración de la cuenta-----")
+            print("Que desea hacer:")
+            print("1. Cambiar contraseña\n2. desactivar la suscripcion\n3. regresar")
+            opcion5=input()
+            if opcion5=="1":
+                usu=input("Escriba el numero de usuario: ")
+                antcontra=input("Escriba la anterior contraseña: ")
+                cursor.execute("select contra from usuario where user_id = %s", (usu))
+                contraant=cursor.fetchone()[0]
+                if contraant == antcontra:
+                    nuecontra=input("Cual es la nueva contraseña: ")
+                    cursor.execute("update usuario set contra=%s where user_id=%s", (nuecontra,usu))
+                    print("Contraseña actualizada exitosamente")
+                else:
+                    print("No coinciden las contraseñas\nVolviendo al menu")
+                    configuracion()
+                configuracion()
+            if opcion5=="2":
+                print("Desea desactivar su suscripcion?")
+                print("1. si\n2. no")
+                opcion51=input()
+                if opcion51 =="1":
+                    print("Se le extrañará, desactivando cuenta...")
+                    usu=input("ingrese el numero de usuario: ")
+                    cursor.execute("update suscripcion set estado='desactivado' where user_id=%s", (usu))
+                    print("Cuenta desactivada exitosamente")
+                    Inicio()
+                if opcion51=="2":
+                    configuracion()
+            if opcion5=="3":
+                Miembro()
+
+def consultas():
+            print("Consultas de:\n1. Calendario\n2. Historial\n3. Asignacion\n4. Regresar")
+            opcion3=input()
+            if opcion3=="1":
+                print("Esto es lo que se tiene en su calendario:")
+                cursor.execute("select * from sesiones")
+                lista=cursor.fetchall
+                print(lista)
+                consultas()
+            if opcion3=="2":
+                print("funcion en construccion\nVolviendo al menu")
+                consultas()
+            if opcion3=="3":
+                asignacion()
+            if opcion3=="4":
+                Miembro()
+
+def busqueda():
+            print("que sesiones desea buscar:")
+            print("1. Por categoria\n2. Por instructor\n3. por duracion\n4. Regresar")
+            opcion2=input()
+            if opcion2=="1":
+                print("Estos son los cursos por categoria")
+                cursor.execute("select * from cursos order by cursos asc")
+                lista=cursor.fetchall()
+                print(lista)
+                busqueda()
+            if opcion2=="2":
+                print("Estos son los curos por instructor:")
+                cursor.execute("select * from cursos order by inst_id asc")
+                lista=cursor.fetchall()
+                print(lista)
+                busqueda()
+            if opcion2=="3":
+                print("Estos son los curos por duracion: ")
+                cursor.execute("select * from cursos order by duracion asc")
+                lista=cursor.fetchall()
+                print(lista)
+                busqueda()
+            if opcion2=="4":
+                Miembro()
+
+def asignacion():
+    print("Asignar un curso")
+    user=input("Ingrese codigo de usuario:")
+    curso=input("Ingrese el codigo del curso al cual se quiere asignar: ")
+    fecha=input("Ingrese la fecha la cual quiere iniciar (formato dia, mes año): ")
+    hora=input
+    cursor.execute("select inst_id from cursos where curso_id = %s", (curso))
+    inst=cursor.fetchone()[0]
+    cursor.execute("select categoria from cursos where curso_id = %s", (curso))
+    categoria=cursor.fetchone()[0]
+    cursor.execute("select duracion from cursos where curso_id = %s", (curso))
+    duracion=cursor.fetchone()[0]
+    cursor.execute("insert into sesiones (curso_id, fecha, hora, duracion, inst_id, categoria, user_id) values (%s, %s, %s, %s, %s, %s, %s)", (curso, fecha, hora, duracion, inst, categoria, user))
+    print("Se ha asignado correctamente!\nDesea asignar otro?")
+    print("1. Si\n2. No")
+    opcion31=input()
+    if opcion31=="1":
+        asignacion()
+    if opcion31=="2":
+        print("Regresando")
+        consultas()
 main()
 
 
